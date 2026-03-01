f4c1389e8920507cc25f6e353e854bc198266c87
supremoxl <supremoxlf@gmail.com>
2026-03-01 12:50:30 -0300

    corrigindo porta render

--- app.py
+++ app.py
@@ -225,4 +225,5 @@ def concluir(id):
     return redirect("/agenda")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
